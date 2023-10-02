d9ccad9861d325a0fdc70cc55f0fe7e2914c2d4a
Lemanski-Wiktor <wiktorlem01@gmail.com>
2023-10-02 20:13:48 +0200

    Install nodemon

--- server/server.js
+++ server/server.js
@@ -1,35 +1,35 @@
-const express = require('express');
-const socketio = require('socket.io');
-const http = require('http');
-const cors = require('cors');
-const path = require('path');
+const express = require("express");
+const socketio = require("socket.io");
+const http = require("http");
+const cors = require("cors");
+const path = require("path");
 
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 const server = http.createServer(app);
 const io = socketio(server, {
-    cors: {
-      origin: '*',
-    }
+  cors: {
+    origin: "*",
+  },
 });
 
 app.use(cors());
 
+io.on("conn", (socket) => {
+  console.log(`New client joined: ${socket.id}`);
 
-io.on('connection', (socket) => {
-    console.log(`New client joined: ${socket.id}`);
+  setTimeout(() => {
+    socket.emit("test", "Event, which was emmited after 5 seconds.");
+  }, 5000);
 
-    setTimeout(() => {
-        socket.emit("test", "Event, which was emmited after 5 seconds.");
-    }, 5000);
+  socket.on("disconnect", () => {});
+});
 
-    socket.on('disconnect', () => {
-        
-    });
-})
-
+app.get("/", (res, req) => {
+  res.send({ name: "Hello World!" });
+});
 
 server.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
+  console.log(`Server running on port ${PORT}`);
+});
