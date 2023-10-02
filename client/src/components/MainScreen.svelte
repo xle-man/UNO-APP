ba72b0ec2b8224e52657e90d9c760d831573f89a
Kamil Zagórski <kamil4.zag@gmail.com>
2023-10-02 12:46:41 +0200

    MainScreen - adding two buttons

---
+++ client/src/components/MainScreen.svelte
@@ -0,0 +1,21 @@
+<script>
+
+    function createGame() {
+
+    }
+
+    function joinGame() {
+
+    }
+
+</script>
+
+<div>
+    <button on:click={createGame}>CREATE</button>
+    <button on:click={joinGame}>JOIN</button>
+</div>
+
+
+<style>
+
+</style>
---
+++ client/src/javascripts/AppStore.js
@@ -0,0 +1,5 @@
+import { writable, get } from 'svelte/store';
+
+export const matchID = writable(null);
+export const playerID = writable(null);
+
