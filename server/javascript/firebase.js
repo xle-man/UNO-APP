a790b35a39103cc8e330d587e7ac8feca9959498
Lemanski-Wiktor <wiktorlem01@gmail.com>
2023-10-03 15:19:02 +0200

    Configure Firebase & Hosting

--- server/javascript/firebase.js
+++ server/javascript/firebase.js
@@ -1,1 +1,12 @@
-// initlize code from firebase here
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAfF3VMXbb0R6MMC8RSM9t1GNpr5Aqfi5k",
+  authDomain: "uno-app-29e17.firebaseapp.com",
+  projectId: "uno-app-29e17",
+  storageBucket: "uno-app-29e17.appspot.com",
+  messagingSenderId: "818758340068",
+  appId: "1:818758340068:web:e803d4b5a6e0b80ffccbf1",
+};
+
+const app = initializeApp(firebaseConfig);
