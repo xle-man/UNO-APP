97b9996a56ad1421fcf48c7c05272c29e2f2ab7f
Kamil Zagórski <kamil4.zag@gmail.com>
2023-10-24 15:52:26 +0200

    generating cards

---
+++ client/src/components/Card.svelte
@@ -0,0 +1,12 @@
+<script>
+    import { get } from "svelte/store";
+    export let color;
+    export let special;
+    export let symbol;
+</script>
+
+<div>
+    Color: {color}
+    Special: {special}
+    Symbol: {symbol}
+</div>
